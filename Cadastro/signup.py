ba7062f4f13285a19e6caa8b115d573af0b13a4c
Andre Wakugawa <andre.wakugawa@outlook.com>
2023-04-05 14:00:37 -0300

    correção de código e add comentários

--- Cadastro/signup.py
+++ Cadastro/signup.py
@@ -1,17 +1,32 @@
+# Input de email, senha e confirmação de senha para cadastro
 email = input("Insira seu e-mail:")
 password = input("Insira sua senha:")
 conf_password = input("Confirme sua senha:")
-lista_cadastro = []
 
-if conf_password == password:
-    print ("Registro bem sucedido!")
+# Teste para verificar se usuário digitou senha corretamente
+while password != conf_password:
+    print ("Senha incorreta, tente novamente!")
+    password = input("Insira sua senha:")
+    conf_password = input("Confirme sua senha:")
 else:
-    print ("Senha incorreta!")
+    print ("Cadastro bem sucedido!")
 
+# Inserção das variáveis dentro de uma lista
+lista_cadastro = []
 lista_cadastro.append(email)
 lista_cadastro.append(password)
 
+# Biblioteca 'csv' para manipulação de arquivos .csv
+# Biblioteca 'os' para manipulação de pastas dentro do windows
+# Biblioteca 'sys' para o algoritmo encontrar o arquivo "cadastro.csv" dentro da própria pasta em que está sendo executado.
 import csv
-with open("cadastro.csv","a", newline='') as csvfile:
+import os
+import sys
+
+# Abertura do arquivo "cadastro.csv" e inserção da lista 'lista_cadastro' em uma nova linha.
+# Importante ressaltar o argumento "newline='' " para impedir a criação de linhas vazias dentro do arquivo .csv.
+# O uso da biblioteca 'os' com 'sys' faz com que o algoritmo atualize o arquivo "cadastro.csv" dentro da pasta,
+# Sem eles um novo arquivo "cadastro.csv" seria criado dentro do CWD (no caso, direto no repo DevMinds).
+with open(os.path.join(sys.path[0],"cadastro.csv"),"a", newline='') as csvfile:
     csvcadastro = csv.writer(csvfile)
     csvcadastro.writerow(lista_cadastro)
