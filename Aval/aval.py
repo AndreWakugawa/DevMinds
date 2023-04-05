79cbc808acd1d60b7e23c90214f306acfa8014c1
Andre Wakugawa <andre.wakugawa@outlook.com>
2023-04-05 12:50:15 -0300

    primeira versão da aval360 e att burndown

---
+++ Aval/aval.py
@@ -0,0 +1,142 @@
+# Inserção inicial dos 5 critérios da avaliação exigidos pela PBLTeX
+# Utiliza-se uma escala de 0 a 4 (5 opções)
+print ("Inicia-se a avaliação 360 da sprint <sprint atual>")
+print ("As notas serão de 0 a 4 sendo \n0: Insatisfatório \n1: Abaixo das expectativas \n2: Satisfatório \n3: Acima das expectativas \n4: Excelente")
+# Avaliação do primeiro fator-chave: Comunicação e Trabalho em equipe
+print ("AVALIAÇÃO 1: COMUNICAÇÃO E TRABALHO EM EQUIPE")
+comun1 = 10
+comunn = 10
+comun0 = 10
+
+while comun1 >4:
+    comun1 = int(input("Em uma escala de 0 a 4, para Comunicação e Trabalho em Equipe, como você avalia <aluno1>?"))
+    if comun1 <= 4:
+        pass
+    else:
+        print ("Escala incorreta, por favor avalie de 0 a 4!")
+
+while comunn >4:
+    comunn = int(input("Em uma escala de 0 a 4, para Comunicação e Trabalho em Equipe, como você avalia <alunoN>?"))
+    if comunn <= 4:
+        pass
+    else:
+        print ("Escala incorreta, por favor avalie de 0 a 4!")
+
+while comun0 >4:
+    comun0 = int(input("Em uma escala de 0 a 4, para Comunicação e Trabalho em Equipe, como você se avalia?"))
+    if comun0 <= 4:
+        pass
+    else:
+        print ("Escala incorreta, por favor avalie de 0 a 4!")
+
+# Avaliação do segundo fator-chave: Engajamento e Pró-atividade
+print ("AVALIAÇÃO 2: ENGAJAMENTO E PRÓ-ATIVIDADE")
+engaj1 = 10
+engajn = 10
+engaj0 = 10
+
+while engaj1 >4:
+    engaj1 = int(input("Em uma escala de 0 a 4, para Engajamento e Pró-atividade, como você avalia <aluno1>?"))
+    if engaj1 <= 4:
+        pass
+    else:
+        print ("Escala incorreta, por favor avalie de 0 a 4!")
+
+while engajn >4:
+    engajn = int(input("Em uma escala de 0 a 4, para Engajamento e Pró-atividade, como você avalia <alunoN>?"))
+    if engajn <= 4:
+        pass
+    else:
+        print ("Escala incorreta, por favor avalie de 0 a 4!")
+
+while engaj0 >4:
+    engaj0 = int(input("Em uma escala de 0 a 4, para Engajamento e Pró-atividade, como você se avalia?"))
+    if engaj0 <= 4:
+        pass
+    else:
+        print ("Escala incorreta, por favor avalie de 0 a 4!")
+
+# Avaliação do terceiro fator-chave: Conhecimento e Aplicabilidade Técnica
+print ("AVALIAÇÃO 3: CONHECIMENTO E APLICABILIDADE TÉCNICA")
+conhec1 = 10
+conhecn = 10
+conhec0 = 10
+
+while conhec1 >4:
+    conhec1 = int(input("Em uma escala de 0 a 4, para Conhecimento e Aplicabilidade Técnica, como você avalia <aluno1>?"))
+    if conhec1 <= 4:
+        pass
+    else:
+        print ("Escala incorreta, por favor avalie de 0 a 4!")
+
+while conhecn >4:
+    conhecn = int(input("Em uma escala de 0 a 4, para Conhecimento e Aplicabilidade Técnica, como você avalia <alunoN>?"))
+    if conhecn <= 4:
+        pass
+    else:
+        print ("Escala incorreta, por favor avalie de 0 a 4!")
+
+while conhec0 >4:
+    conhec0 = int(input("Em uma escala de 0 a 4, para Conhecimento e Aplicabilidade Técnica, como você se avalia?"))
+    if conhec0 <= 4:
+        pass
+    else:
+        print ("Escala incorreta, por favor avalie de 0 a 4!")
+
+# Avaliação do quarto fator-chave: Entrega de Resultados com Valor Agregado
+print ("AVALIAÇÃO 4: ENTREGA DE RESULTADOS COM VALOR AGREGADO")
+result1 = 10
+resultn = 10
+result0 = 10
+
+
+while result1 >4:
+    result1 = int(input("Em uma escala de 0 a 4, para Entrega de Resultados com Valor Agregado, como você avalia <aluno1>?"))
+    if result1 <= 4:
+        pass
+    else:
+        print ("Escala incorreta, por favor avalie de 0 a 4!")
+
+while resultn >4:
+    resultn = int(input("Em uma escala de 0 a 4, para Entrega de Resultados com Valor Agregado, como você avalia <alunoN>?"))
+    if resultn <= 4:
+        pass
+    else:
+        print ("Escala incorreta, por favor avalie de 0 a 4!")
+
+while result0 >4:
+    result0 = int(input("Em uma escala de 0 a 4, para Entrega de Resultados com Valor Agregado, como você se avalia?"))
+    if result0 <= 4:
+        pass
+    else:
+        print ("Escala incorreta, por favor avalie de 0 a 4!")
+
+# Avaliação do quinto fator-chave: Auto-gestão das Atividades
+print ("AVALIAÇÃO 5: AUTO-GESTÃO DAS ATIVIDADES")
+autogestao1 = 10
+autogestaon = 10
+autogestao0 = 10
+
+while autogestao1 >4:
+    autogestao1 = int(input("Em uma escala de 0 a 4, para Auto-gestão das Atividades, como você avalia <aluno1>?"))
+    if autogestao1 <= 4:
+        pass
+    else:
+        print ("Escala incorreta, por favor avalie de 0 a 4!")
+
+while autogestaon >4:
+    autogestaon = int(input("Em uma escala de 0 a 4, para Auto-gestão das Atividades, como você avalia <alunoN>?"))
+    if autogestaon <= 4:
+        pass
+    else:
+        print ("Escala incorreta, por favor avalie de 0 a 4!")
+
+while autogestao0 >4:
+    autogestao0 = int(input("Em uma escala de 0 a 4, para Auto-gestão das Atividades, como você se avalia?"))
+    if autogestao0 <= 4:
+        pass
+    else:
+        print ("Escala incorreta, por favor avalie de 0 a 4!")
+
+# Fim da avaliação
+print ("Avaliação finalizada!!!")
