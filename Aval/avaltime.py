859db124ca8aa882042a42b3c07a175b290d9f96
Gabriel Guska <gabrielguska123@gmail.com>
2023-04-07 16:37:53 -0300

    Codigo da avaliação junto com o calculo das médias para teste do calculo das medias

---
+++ Aval/avaltime.py
@@ -0,0 +1,51 @@
+#CODIGO PARA TESTE DE MEDIAS LINKADO COM O DA AVALIAÇÃO
+
+#LINKAR LISTAS COM O ARQUIVO CSV > COMUNICAÇÃO, ENGAJAMENTO, CONHECIMENTO, RESULTADO E AUTOGESTÃO
+
+# Inserção inicial dos 5 critérios da avaliação exigidos pela PBLTeX
+# Utiliza-se uma escala de 0 a 4 (5 opções)
+import statistics
+print("------AVALIAÇÃO DO TIME------\n\nAs notas serão de 0 a 4 sendo:")
+print("\n0: Insatisfatório \n1: Abaixo das expectativas \n2: Satisfatório \n3: Acima das expectativas \n4: Excelente\n")
+
+fatores_chave = ["Comunicação e Trabalho em Equipe", "Engajamento e Pró-atividade", "Conhecimento e Aplicabilidade Técnica", "Entrega de Resultados com Valor Agregado", "Auto-gestão das Atividades"]
+
+avaliacoes = []
+notas_aluno1 = []
+notas_aluno2 = []
+notas_aluno3 = []
+
+# Puxar ID dos integrantes pelo arquivo CSV
+integrantes = ["Aluno 1", "Aluno 2", "Aluno 3"]
+
+for fator in fatores_chave:
+    print(f"AVALIAÇÃO: {fator}\n")
+    for i, pessoa in enumerate(integrantes):
+        avaliacao = 10
+        while avaliacao > 4:
+            avaliacao = int(input(f"Em uma escala de 0 a 4, para {fator}, como você avalia o {pessoa}?\n"))
+            if avaliacao > 4:
+                print("Escala incorreta, por favor avalie de 0 a 4!\n")
+        if i == 0:
+            notas_aluno1.append(avaliacao)
+        elif i == 1:
+            notas_aluno2.append(avaliacao)
+        else:
+            notas_aluno3.append(avaliacao)
+
+media_aluno1 = statistics.mean(notas_aluno1)
+media_aluno2 = statistics.mean(notas_aluno2)
+media_aluno3 = statistics.mean(notas_aluno3)
+
+while True:
+    aluno = int(input("Qual aluno você deseja ver a média?\n(1) Aluno 1 (2) Aluno 2 (3) Aluno 3\n(4) Sair\n"))
+    if aluno == 1:
+        print (f"A média do aluno 1 é: {media_aluno1}")
+    elif aluno == 2:
+        print (f"A média do aluno 2 é: {media_aluno2}")
+    elif aluno == 3:
+        print (f"A média do aluno 3 é: {media_aluno3}")
+    elif aluno == 4:
+        break
+    else:
+        print("Aluno inexistente.")
