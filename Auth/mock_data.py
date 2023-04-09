c5fe4b2a173da749d711f246eba8c82ffc246634
sam <samantharlnogueira@gmail.com>
2023-04-08 22:07:16 -0300

    feat: get colegas de time

---
+++ Auth/mock_data.py
@@ -0,0 +1,12 @@
+mock_users = [
+
+  {"nome": "Juquinha", "time": "fake"},
+  {"nome": "Joaninha", "time": "fake"},
+  {"nome": "Clovis", "time": "fake"},
+  {"nome": "Laura", "time": "fake"},
+
+  {"nome": "Mariazinha", "time": "fake2"},
+  {"nome": "Karen Bachini", "time": "fake2"},
+  {"nome": "Ronaldo", "time": "fake2"},
+  {"nome": "Jadisson", "time": "fake2"},
+]
