0cdc558e55ac519163c003e834ffc0eae4e5340c
Andre Wakugawa <andre.wakugawa@outlook.com>
2023-04-20 11:43:29 -0300

    feat: atualizacao da aval no csv. change: limpeza de codigo no import do csv
    a função de atualizacao recria o csv em uma lista com sublistas dentro do python.
    cria-se uma lista 'notasaluno' com o resultado da avaliacao referente ao aluno avaliado.
    nisso entramos no loop while que checa se 'notasaluno' está vazia e também estabelece um
    limite no loop para o numero de integrantes da equipe (nesse caso 3).
    
    dentro do loop while entramos em um loop for que checa a coluna de notas para o aluno avaliado
    e atualiza os valores de acordo com o primeiro valor de 'notasaluno' referente ao primeiro criterio.
    no fim do loop for com o metodo 'pop(0)' elimina-se o primeiro elemento de 'notasaluno' e partimos
    para o segundo elemento, até a lista se esvaziar e a tabela ser preenchida com os valores dos 5
    criterios para o primeiro aluno da equipe (nesse caso 1001).
    
    saindo do loop for adicionamos += 1 ao index de 'notasaluno' e de 'integrantesindex' para atualizar
    os dados para o proximo aluno alem de atualizar as condicoes do loop 'for' para o novo aluno.
    atualiza-se a lista 'notasaluno' com os novos dados e o loop recomeça.
    
    se o index de alunos ultrapassar o index das sublistas da aval (mais alunos do que notas)
    o loop quebra e finaliza.
    
    o metodo 'seek(0)' retorna a leitura do csv para o inicio,
    sem ele o script criaria novas linhas e nao atualizaria as ja existentes
    
    por fim escrevemos em cima das informacoes originais do csv, mantendo as que nao entraram nas condicoes
    e atualizando as que entraram nas condicoes.

--- Aval/aval.py
+++ Aval/aval.py
@@ -16,25 +16,28 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 # Identificar o caminho do csv relativo a pasta do script:
 csv_path = os.path.join(script_dir, 'aval.csv')
 
-# Abrir o arquivo csv para LER e extrair os usuários do time na turma (NESSE CASO TIME 1 DA TURMA 1!!!)
+# Abrir o arquivo csv para LER e extrair os usuários do time na turma (NESSE CASO SPRINT 1 DO TIME 1 DA TURMA 1!!!)
 with open(csv_path, 'r') as avalcsv:
     avalread = csv.reader(avalcsv)
 
     # Pegar o header do csv
     header = next(avalread)
 
-    # Definir a coluna para encontrar a turma
-    turma = "turma"
-    colunaturma = header.index(turma)
+    # Definir as colunas do csv
+    colunaturma = header.index('turma')
+    colunatime = header.index('time')
+    colunasprint = header.index('sprint')
+    colunaaluno = header.index('aluno')
+    colunanotas = header.index('notas')
 
     # Pesquisar por linha
     for linha in avalread:
-        # Checar igualdade entre coluna turma e código desejado, nesse caso: 1; E chega time e código desejado, nesse caso também 1
+        # Checar igualdade entre coluna turma, time e sprint desejado, nesse caso: 1, 1 e 1
         ### !!! ENCONTRAR UM JEITO DE PEGAR O CÓDIGO DA TURMA ATRAVÉS DO USUÁRIO QUE LOGOU PARA AVALIAR !!!
-        if linha[colunaturma] == "1" and linha[colunaturma + 1] == "1":
+        if linha[colunaturma] == "1" and linha[colunatime] == "1" and linha[colunasprint] == "1":
             # Com a igualdade, adicionar os integrantes da quinta coluna, 'aluno' (coluna 1 + 4) que forem do da turma 1, time 1
-            if linha[colunaturma + 4] not in integrantes:
-                integrantes.append(linha[colunaturma + 4])
+            if linha[colunaaluno] not in integrantes:
+                integrantes.append(linha[colunaaluno])
 
 
 
@@ -71,3 +74,65 @@ print ("Avaliação Finalizada!!!")
 
 # Somente para visualizar os inputs salvos na variável
 print (avaliacoes)
+
+
+
+
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+# Salvando os inputs da aval dentro do aval.csv
+
+with open(csv_path, 'r+', newline='') as avalcsv:
+    avalread = csv.reader(avalcsv)
+    # Carrega os dados originais do csv
+    dados = [initlinha for initlinha in avalread]
+
+    integrantesindex = 0
+    notasalunoindex = 0
+
+
+    notasaluno = [avalnota[notasalunoindex] for avalnota in avaliacoes]
+
+    while notasaluno and notasalunoindex <= len(avaliacoes[0]):
+
+    # Update dos dados dentro das condições para a add de notas
+        for linha in dados:
+
+            if linha[0] == "1" and linha[1] == "1" and linha[2] == "1":
+            
+                if linha[colunaaluno] == integrantes[integrantesindex]:
+                # confere e remove se tiver um espaço em branco antes de add
+                    notas = linha[5].strip()
+                # se a celula estiver vazia add o valor
+                    if notas == "":
+                        notas += str(notasaluno[0])
+                # se a celula estiver algo dentro, add uma virgula antes de add valor
+                    else:
+                        notas += "," + str(notasaluno[0])
+                    if linha[5].startswith('"') and linha[5].endswith('"'):
+                        pass
+                    else:
+                        linha[5] = notas
+                # METODO pop() MARAVILHOSO QUE ENCAIXOU CERTINHO PRO QUE EU QUERIA
+                    notasaluno.pop(0)
+        if len(notasaluno) == 0:
+            notasalunoindex += 1
+            integrantesindex += 1
+            if notasalunoindex >= len(avaliacoes[0]):
+                break
+            notasaluno = [avalnota[notasalunoindex] for avalnota in avaliacoes]
+
+    
+    # Volta ao inicio do csv para reescrever os novos dados sobre o csv original
+    # A leitura estava na ultima linha do csv após a passagem do loop for
+    avalcsv.seek(0)
+
+    avalwriter = csv.writer(avalcsv)
+    avalwriter.writerows(dados)
+
+    # Remove tudo após os writes para manter o arquivo limpo
+    avalcsv.truncate()
+    # Fecha o open para evitar acessos concorrentes
+    avalcsv.close()
