cc5964c6e033a4d4d8faff9bf5fb46f7feb852ea
CauevSilv <caue.silva10@fatec.sp.gov.br>
2023-05-04 19:31:30 -0300

    Remake da EvalCad e suporte ao UTF-8
    Refiz todo o módulo evalcad, agora utilizando o CSV ao invés do Pandas.
    O Suporte a UTF-8 permite que qualquer caractere seja lido como qualquer outra string, sem criar aqueles losangos com interrogação. (UTF-8 já está em todos os módulos.

--- packages/signup_module.py
+++ packages/signup_module.py
@@ -1,5 +1,6 @@
 def add_cad():
     from Main import os, sys, csv, pw, uuid # Puxa imports da main
+    from packages.evalcad_module import evalcad
     filename = 'login_data.csv' # Nome do arquivo
     target_dir = 'Data_Sample/' # Diretório relativo
     csv_path = os.path.join(target_dir, filename) # Seleciona diretório
@@ -24,6 +25,7 @@ def add_cad():
     # Inserção das variáveis dentro de uma lista
 
     # Sem um novo arquivo "cadastro.csv" seria criado dentro do CWD (no caso, direto no repo DevMinds).
-    with open(csv_path,'a', newline='') as cad_csv: # Abre csv, inserindo uma nova linha
+    with open(csv_path,'a', newline='',encoding='utf-8') as cad_csv: # Abre csv, inserindo uma nova linha
         csv_writer = csv.writer(cad_csv) # Objeto de escrito do csv
         csv_writer.writerow([id, nome, email, senha])
+    evalcad()
