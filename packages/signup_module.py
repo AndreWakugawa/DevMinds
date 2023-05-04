3510d119e24d2cdc47a92a8042ff02d6179c9f76
avilajp <avilassp@gmail.com>
2023-05-04 19:23:24 -0300

    Validação de e-mail de cadastro de usuário

--- packages/signup_module.py
+++ packages/signup_module.py
@@ -1,3 +1,5 @@
+import re
+
 def add_cad():
     from Main import os, sys, csv, pw, uuid # Puxa imports da main
     filename = 'login_data.csv' # Nome do arquivo
@@ -9,8 +11,16 @@ def add_cad():
     uuidFour = uuid.uuid4()
     id = uuidFour
 
-    # Teste para verificar se usuário digitou senha corretamente
-    email = input("Insira seu e-mail: ")
+    # Expressão regular para validar e-mail
+    email_regex = r'^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$'
+    
+    while True:
+        email = input("Insira seu e-mail: ")
+        if re.match(email_regex, email):
+            break
+        else:
+            print("E-mail inválido. Por favor, insira um e-mail no formato 'usuario@dominio.com'.")
+        
     nome = input("Insira seu nome: ")
     while True:
         senha = pw.pwinput("Crie uma senha: ")
