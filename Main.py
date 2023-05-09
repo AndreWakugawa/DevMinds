f69937a0f74cd519b3b95efc8039fdf9a088fbdd
CauevSilv <caue.silva10@fatec.sp.gov.br>
2023-05-09 20:14:03 -0300

    Revert "implementação da função de poder de usuario"
    This reverts commit 35f76bc48658ad453737ae60d28c5c045c0033c2.

--- Main.py
+++ Main.py
@@ -12,58 +12,33 @@ from packages.login_module import login_check
 from packages.eval_module import eval
 from packages.signup_module import add_cad
 
-while True:
+while True: # Loop do login até ser valido
     id = []
     nome = []
-    login_info = login_check(id, nome)
-    if login_info:
-        id, nome = login_info
-        file_path = os.path.abspath('data_sample/login_data.csv')
-
-        with open(file_path, mode='r') as csv_file:
-            level_reader = csv.reader(csv_file, delimiter=',')
-            next(level_reader, None) # Ignora headers
-            for linha in level_reader:
-                if linha[0] == id:
-                    if linha[4] == '1':
-                        user_level = 1
-                    elif linha[4] == '0':
-                        user_level = 0  # Checa o nível do usuário
-            break
+    login_info = login_check(id, nome) # Puxa função
+    if login_info: # Checa se é verdadeiro
+        break
     print("Dados de Login ou Senha incorretos, tente novamente")
 
-opcoes_aluno = ['Realizar avaliação', 'Sair']
-opcoes_adm = ['Gerenciar cadastros', 'Realizar avaliação', 'Analisar dashboards', 'Sair']
-
-if user_level == 0:
-    while True:
-        print(f"Olá, {nome}! O que deseja fazer?\n")
-        print(f"1. {opcoes_aluno[0]}\n2. {opcoes_aluno[1]}")
-        opcao = input("Insira o número da opção: ")
-        if opcao == "1":
-            eval()
-        elif opcao == "2":
-            sair = input("Deseja mesmo sair? (s/n)")
-            if sair == "s":
-                sys.exit()
-        else:
-            print("Opção inválida!")
-            break
-elif user_level == 1:
-    while True:
-        print(f"Olá, {nome}! O que deseja fazer?\n")
-        print(f"1. {opcoes_adm[0]}\n2. {opcoes_adm[1]}\n3. {opcoes_adm[2]}\n4. {opcoes_adm[3]}")
-        opcao = input("Insira o número da opção: ")
-        if opcao == "1":
-            add_cad()
-        elif opcao == "2":
-            eval()
-        elif opcao == "3":
-            print("\nDashboards em desenvolvimento\n")
-        elif opcao == "4":
-            sair = input("Deseja mesmo sair? (s/n)")
-            if sair == "s":
-                sys.exit()
-        else:
-            print("Opção inválida!")
-            break
+while True: # Loop de opções
+    id, nome = login_info
+    opções =['Gerenciar cadastros','Realizar avaliação','Analisar dashboards','Sair']
+    
+    print(f'\nOlá! {nome}, o que deseja fazer?\n')
+    print (f'1.{opções[0]}\n2.{opções[1]}\n3.{opções[2]}\n4.{opções[3]}')
+    
+    opção = int(input("Insira o número da opção:"))
+    
+    if opção == 1:
+        add_cad() # Funcao cadastro
+    elif opção == 2:
+        eval() # Funcao avaliacao
+    elif opção == 3:
+        print ('\nDashboards em desenvolvimento\n') # Funcao dashboards
+    elif opção == 4:
+        sair = input('Deseja mesmo sair?(y/n)')
+        if sair in ['y']:
+            sys.exit() # Termina execucao
+    else:
+        print('Opção inválida!')
+        break
