dd71cd09e77a44009aafa9f9549f66c360b1b3b4
CauevSilv <caue.silva10@fatec.sp.gov.br>
2023-05-21 15:45:40 -0300

    Salvar Avaliação
    Agora a aval_module salva efetivamente a avaliação realizada no BD.
    Foi criado uma lista nova em que, no final da aval, é somada nela a lista avaliacoes e feedbacks.
    Fiz várias paradas e bagulhetes para ela salvar bonitinho no banco, pq ele trata como lista o que é... lista.
    O evalDB.csv SEMPRE deve ser salvo sendo a ultima linha uma linha vazia com o ponteiro nela. SEMPRE.

--- packages/aval_module.py
+++ packages/aval_module.py
@@ -7,6 +7,7 @@ def aval(id_user, nome):
     integrantes_id = {} # dict nome:id_user
     turma = []
     time = []
+    inputDB = [] #A nova linha inputada no DB. Obrigatoriamente segue a ordem dos Headers existentes lá.
 
     with open('usersDB.csv', 'r', newline='', encoding='utf-8') as user_csv:
         user_reader = csv.reader(user_csv)
@@ -20,9 +21,9 @@ def aval(id_user, nome):
         user_csv.seek(0)
         for linha_integrante in user_reader:
             if linha_integrante[2] == turma and linha_integrante[1] == time: #Checa se turma e time batem
-                integrantes_info.append(linha_integrante[0]) # Junta nomes em lista
-                integrantes_nome.append(linha_integrante[5])
-                integrantes_id[linha_integrante[5]] = linha_integrante[0]
+                integrantes_info.append(linha_integrante[0]) # Junta IDs em lista
+                integrantes_nome.append(linha_integrante[5]) # Junta nomes em lista ???
+                integrantes_id[linha_integrante[5]] = linha_integrante[0]   # Cria uma lista assim : {'Nome': 'ID'}. Exemplo {'Cleber': '2'}
 
             #dados = [csvlinha for csvlinha in reader_obj] # reescrita do csv dentro de lista em python
             #fb_coluna_index = 10 # Coluna inicial dos feedbacks no aval_data.csv
@@ -35,8 +36,17 @@ def aval(id_user, nome):
                         "Conhecimento e Aplicabilidade Técnica",
                         "Entrega de Resultados com Valor Agregado",
                         "Auto-gestão das Atividades"]
-        
-        
+        newidAval = None
+
+        with open('evalDB.csv', 'r',encoding='utf-8') as addID:
+            readeridnew = csv.reader(addID)
+            idnew = 0
+            for idold,row in enumerate(readeridnew):
+                if idold == 0:
+                    continue
+                last_id = int(row[0])
+            idnew = last_id + 1
+            inputDB.append(idnew)
 
         
         
@@ -69,7 +79,8 @@ def aval(id_user, nome):
                                     break
                             except ValueError:
                                 print("Valor inválido\n")
-                        avaliacoes.append(avaliacao)
+                        avaliacoes.append(int(avaliacao))
+                        #avaliacoes.append(avaliacao)
                         # Feedback caso a avaliação seja entre 1 e 3
                         if int(avaliacao) in range(1,4):
                             feedback_confirm = ""
@@ -78,22 +89,30 @@ def aval(id_user, nome):
                                 feedback_confirm = input("Você confirma o feedback?(y/n)\n")
                                 if feedback_confirm != "y":
                                     print ("Tente novamente:")
-                        feedbacks.append(feedback)  # Registro do feedback
-                        print(avaliacoes,feedbacks)
+                                else:
+                                    feedbacks.append(feedback)  # Registro do feedback
+                        else:
+                            feedback = None
+                            feedbacks.append(feedback)# Registro do feedback vazio       
+                        print(inputDB)
                         # Nos appends juntam-se as notas dadas em uma lista
+                    inputDB.append(int(integrantes_id[integrante]))
+                    inputDB.append(int(time))
+                    inputDB.append(int(turma))
+                    inputDB.append("Ainda não implementado")
+                    inputDB.extend(avaliacoes) #Adição da lista avaliacoes na linha virtual.
+                    inputDB.extend(feedbacks) #Adição da lista feedbackas na linha virtual.
                     print("Avaliação Finalizada!!!")
                     print(f'{integrante} foi avaliado(a)!!\n{avaliacoes}') # Visualiza notas dadas 
                     break
             else:
                 print('Integrante não encontado, tente novamente.')
                 break
+    row = [str(item) for item in inputDB]
 
-        
-        #csv_writer= csv.writer(evalDB) # Objeto de escrito do csv
-        #csv_writer.writerow(avaliacoes)
-        #evalDB.seek(0)
-        #avalwriter = csv.writer(evalDB)
-        #avalwriter.writerows(dados)
-        # Remove tudo após os writes para manter o arquivo limpo
-        #evalDB.truncate()
-        #Avaliação terminada, falta adicao de notas ao csv
+    # Criar uma única string com os elementos separados por vírgulas
+    csv_row = ','.join(row)
+
+    # Gravar a linha no arquivo CSV
+    with open('evalDB.csv', 'a', newline='', encoding='utf-8') as cad_csv:
+        cad_csv.write(csv_row + '\n')
